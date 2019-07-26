83e2012d507589111712215fae4b98fd6a81dd62
Robel <robelseyoum3@gmail.com>
2019-07-26 13:03:01 +0100

    Implemented add functionality

--- app/src/main/java/com/robelseyoum3/calculator/MainActivity.kt
+++ app/src/main/java/com/robelseyoum3/calculator/MainActivity.kt
@@ -2,11 +2,32 @@ package com.robelseyoum3.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_save.setOnClickListener {
+            tv_user_name.text = "Hello " + et_name.text.toString()
+        }
+//
+//        String numOne = etNumberOne.getText().toString();
+//        String numTwo = etNumberTwo.getText().toString();
+//
+//        int a = Integer.parseInt(numOne);
+//        int b = Integer.parseInt(numTwo);
+//
+//        int result = a + b;
+//        tvResult.setText(Integer.toString(result));
+
+
+
+        btn_add.setOnClickListener {
+            //tv_result.text = et_number_one.text.toString().toInt() + et_number_two.text.toString().toInt()
+        }
+
     }
 }
---
+++ app/src/main/java/com/robelseyoum3/calculator/MainJavaActivity.java
@@ -0,0 +1,55 @@
+package com.robelseyoum3.calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainJavaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnSave = findViewById(R.id.btn_save);
+        final EditText etName = findViewById(R.id.et_name);
+        final TextView tvName = findViewById(R.id.tv_user_name);
+
+
+        final EditText etNumberOne = findViewById(R.id.et_number_one);
+        final EditText etNumberTwo = findViewById(R.id.et_number_two);
+
+        Button btnAdd = findViewById(R.id.btn_add);
+        final TextView tvResult = findViewById(R.id.tv_result);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = etName.getText().toString();
+                tvName.setText("Hello "+name);
+            }
+        });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String numOne = etNumberOne.getText().toString();
+                String numTwo = etNumberTwo.getText().toString();
+
+                int a = Integer.parseInt(numOne);
+                int b = Integer.parseInt(numTwo);
+
+                int result = a + b;
+                tvResult.setText(Integer.toString(result));
+
+            }
+        });
+
+    }
+
+}
